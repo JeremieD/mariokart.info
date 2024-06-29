27d948019ca05a51b315bbdb6dc7977c45c3d483
jej <5505771+jjjejjj@users.noreply.github.com>
2024-06-28 20:12:56 -0400

    Make tabs navigable using arrow keys

--- public/resources/scripts/tabs.js
+++ public/resources/scripts/tabs.js
@@ -1,19 +1,34 @@
 whenDOMReady(() => {
-  for (const tabContainer of document.getElementsByClassName("tab-container")) {
-    for (const handle of tabContainer.querySelectorAll(".tab-handles>button")) {
-      const handler = e => {
-        e.preventDefault();
-        const tabID = handle.dataset.tabId;
-        for (const handle of tabContainer.querySelectorAll(".tab-handles>button")) {
-          handle.classList.toggle("selected", handle.dataset.tabId == tabID);
-        }
-        for (const panel of tabContainer.querySelectorAll(".tab-panels>*")) {
-          panel.classList.toggle("selected", panel.id == tabID);
-        }
-      };
-      handle.addEventListener("click", handler);
+  const tabContainers = document.getElementsByClassName("tab-container");
+  for (const tabContainer of tabContainers) {
+    const handles = tabContainer.querySelectorAll(".tab-handles>[data-tab-id]");
+    const panels = tabContainer.querySelectorAll(".tab-panels>*");
+
+    function selectTab(id) {
+      for (const handle of handles) {
+        handle.classList.toggle("selected", handle.dataset.tabId == id);
+      }
+      for (const panel of panels) {
+        panel.classList.toggle("selected", panel.id == id);
+      }
+      tabContainer.querySelector("[data-tab-id=\"" + id + "\"]").focus();
+    }
+
+    for (let i = 0; i < handles.length; i++) {
+      const handle = handles[i];
+      handle.addEventListener("click", e => {
+        selectTab(handle.dataset.tabId);
+      });
       handle.addEventListener("keydown", e => {
-        if (e.key == "Enter") handler(e);
+        if (e.key == "Enter") clickHandler(e);
+        if (e.key == "ArrowLeft") {
+          const prevId = i == 0 ? handles.length - 1 : i - 1;
+          selectTab(handles[prevId].dataset.tabId);
+        }
+        if (e.key == "ArrowRight") {
+          const nextId = i == handles.length - 1 ? 0 : i + 1;
+          selectTab(handles[nextId].dataset.tabId);
+        }
       });
     }
   }
