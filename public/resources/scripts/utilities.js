9d4d2e99930b837cb13581e3f109e8ab327caeca
jej <5505771+jjjejjj@users.noreply.github.com>
2024-07-04 16:54:32 -0400

    Fixes
    - add cache to httpGet function
    - fix unstable scrollbars in windows
    - fix being unable to close credits dialog with escape

--- public/resources/scripts/utilities.js
+++ public/resources/scripts/utilities.js
@@ -15,26 +15,31 @@ function whenDOMReady(callback, options = { once: true, passive: true }) {
  */
 async function httpGet(url) {
   return new Promise(function(resolve, reject) {
+    const cacheHit = httpGetCache[url];
+    if (cacheHit && Date.now() - cacheHit.time < 86400000) { // 1 day
+      return resolve(httpGetCache[url].data);
+    }
     const httpRequest = new XMLHttpRequest();
-
     httpRequest.onreadystatechange = () => {
       if (httpRequest.readyState === XMLHttpRequest.DONE) {
-
         if (httpRequest.status === 200) {
           try {
+            httpGetCache[url] = {
+              time: Date.now(),
+              data: httpRequest.responseText
+            };
             resolve(httpRequest.responseText);
           } catch (e) {
             console.error(e);
             reject(e);
           }
-
         } else {
           reject(httpRequest.status);
-        }
-      }
+      } }
     };
-
     httpRequest.open("GET", url);
     httpRequest.send();
   });
 }
+// { url: { time, data }, ... }
+const httpGetCache = {};
