fff5f408593c448a83bb15624cca8e831e303b2f
jej <5505771+jjjejjj@users.noreply.github.com>
2025-09-06 15:47:02 -0400

    Fix widget transitions visible on dialog open

--- public/resources/scripts/mk8dx/view.js
+++ public/resources/scripts/mk8dx/view.js
@@ -1351,6 +1351,7 @@ function drawFormulaDialog() {
   V.formula.dialog.inert = false;
   disableScroll(document.documentElement);
   if (V.formula.dialog.open) return;
+  blockAnimation(V.formula.dialog);
   V.formula.dialog.showModal();
   V.formula.dialog.scrollTop = state.formulaDialogScrollTop;
 }
@@ -1439,6 +1440,7 @@ function drawSettingsDialog() {
 
   V.settings.dialog.inert = false;
   if (V.settings.dialog.open) return;
+  blockAnimation(V.settings.dialog);
   V.settings.dialog.showModal();
 }
 
