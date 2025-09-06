8bc414ba3addd134728f521f206bfbdd34b9fca1
jej <5505771+jjjejjj@users.noreply.github.com>
2025-09-06 15:51:30 -0400

    Move some general duplicated functions in utils

--- public/resources/scripts/utilities.js
+++ public/resources/scripts/utilities.js
@@ -59,7 +59,30 @@ window.structuredClone ??= obj => {
   return JSON.parse(JSON.stringify(obj));
 };
 
+// Returns whether the click event *e* is inside element *el*.
+function isOutside(el, e) {
+  const rect = el.getBoundingClientRect();
+  // If the click event was fired with a keyboard press, e.detail will be 0.
+  if (e.detail === 0) {
+    // In that case, clientX and Y will be 0, so we use the target element's position instead.
+    const target = e.target.getBoundingClientRect();
+    return target.top < rect.top   || target.bottom > rect.bottom
+        || target.left < rect.left || target.right > rect.right;
+  }
+  return e.clientY < rect.top  || e.clientY > rect.bottom
+      || e.clientX < rect.left || e.clientX > rect.right;
+}
+
+function disableScroll(el) { el.classList.add("no-scroll"); }
+function enableScroll(el)  { el.classList.remove("no-scroll"); }
+
 function blockAnimation(el) {
   el.classList.add("no-animation");
   setTimeout(() => { el.classList.remove("no-animation"); }, 500);
+}
+
+function parseValue(v, defaultV = 0) {
+  v = parseFloat(v);
+  if (isNaN(v)) return parseFloat(defaultV);
+  return v;
 }
