9e666a3f07ce8ca622cd80d6402d2631bd42e917
Jérémie Dupuis <JeremieD@users.noreply.github.com>
2025-09-28 12:20:40 -0400

    Add more tests

--- public/resources/scripts/mkw/tests.js
+++ public/resources/scripts/mkw/tests.js
@@ -53,24 +53,61 @@ state.parts.then(parts => {
 // Test important combos' stats
 testStats("mario", "std", [2, 2.2, 1.8, 2, 1]); // Mario Std
 testStats("mario", "dorrie", [2.2, 2.2, 2.2, 1.4, 1]);
+testStats("luigi", "rally", [2.2, 2, 2, 1.8, 1]);
 testStats("peach", "plush", [1.8, 2.4, 1.6, 2.2, 1]);
+testStats("daisy", "rallyBike", [1.2, 3, 1.2, 2.6, 1]);
 testStats("yoshi", "frog", [2, 2.2, 1.8, 2, 1]);
+testStats("dk", "truck", [3, 1, 2.8, 1.2, 2]);
 testStats("bowser", "bruiser", [3.2, .8, 3, 1, 2]);
 testStats("bowser", "std", [2.6, 1.6, 2.4, 1.4, 2]);
 testStats("bowser", "dorrie", [2.6, 1.6, 2.8, 1, 2]);
+testStats("bowser", "dasher", [2.6, 1.4, 2.6, 1.4, 2]); // Heaviest adjust
 testStats("bowserJr", "trike", [2.4, 1.6, 2.2, 1.8, 1]);
+testStats("koopa", "pipeBike", [1, 3.2, 1, 2.8, 0]);
 testStats("toad", "dump", [2.2, 1.8, 2, 2, 0]);
+testStats("toadette", "loco", [1.4, 2.8, 1.2, 2.6, 0]);
 testStats("lakitu", "cloud", [1.8, 2.4, 1.6, 2.2, 0]);
 testStats("kingboo", "reel", [2.8, 1.4, 2.2, 1.6, 2]);
+testStats("shyguy", "rod", [1.8, 2.4, 1.6, 2.2, 0]);
 testStats("shyguy", "carpet", [1.8, 2.4, 1.6, 2.2, 0]);
 testStats("wario", "chopper", [2.8, 1.4, 2.2, 1.6, 2]);
-testStats("wario", "chopper", [2.8, 1.4, 2.2, 1.6, 2]);
+testStats("waluigi", "chopper", [2.8, 1.4, 2.2, 1.6, 2]);
+testStats("waluigi", "gator", [2.8, 1.4, 3, .8, 2]);
+testStats("birdo", "royale", [2.2, 2, 2, 1.8, 1]);
+testStats("pauline", "bill", [2.6, 1.4, 2.6, 1.4, 2]);
 testStats("rosalina", "starSled", [3, 1, 2.6, 1.4, 2]);
-testStats("biddy", "biddy", [1, 3.4, .8, 2.8, 0]);
-testStats("daisyBb", "zoom", [1.4, 2.8, 1.2, 2.6, 0]);  // Lightest adjust
-testStats("bowser", "dasher", [2.6, 1.4, 2.6, 1.4, 2]); // Heaviest adjust
+testStats("marioBb", "pipe", [.8, 3.4, 1, 2.8, 0]);
+testStats("luigiBb", "blast", [1.4, 3, 1.4, 2.2, 0]);
+testStats("peachBb", "titan", [2, 2.2, 1.6, 2.2, 0]);
+testStats("daisyBb", "zoom", [1.4, 2.8, 1.2, 2.6, 0]); // Lightest adjust
+testStats("daisyBb", "scoot", [.6, 3.8, .6, 3, 0]);
+testStats("rosalinaBb", "biddy", [1, 3.2, 1, 2.8, 0]);
+testStats("nabbit", "junk", [2.2, 2.2, 1.8, 1.8, 0]);
+testStats("goomba", "pipeBike", [.8, 3.4, .8, 3, 0]);
+testStats("drybones", "stdBike", [.6, 3.6, .8, 3, 0]);
+testStats("piranha", "horn", [2.6, 1.4, 2.6, 1.4, 2]);
+testStats("spike", "gator", [1.8, 2.4, 2, 1.8, 0]);
+testStats("wiggler", "machBike", [2, 2.2, 1.8, 2, 2]);
+testStats("hammerbro", "zoom", [2.2, 2, 2, 1.8, 1]);
+testStats("crab", "lobster", [2, 2.2, 2, 1.8, 0]);
+testStats("cataquack", "pipe", [1.6, 2.6, 1.8, 2, 2]);
+testStats("mole", "trike", [2.4, 1.6, 2.2, 1.8, 1]);
+testStats("cheep", "fin", [1, 3.2, 1, 2.8, 0]);
+testStats("pianta", "royale", [2.6, 1.6, 2.4, 1.4, 2]);
+testStats("pianta", "dorrie", [2.6, 1.8, 2.6, 1, 2]);
+testStats("wrench", "pipeBike", [1.4, 2.8, 1.4, 2.4, 1]);
+testStats("conkdor", "bloop", [2, 2.2, 1.8, 2, 2]);
+testStats("swoop", "chopper", [1.8, 2.6, 1, 2.6, 0]);
+testStats("pokey", "dreadSled", [2.2, 1.8, 2, 2, 1]);
+testStats("peepa", "rob", [1, 3.2, .8, 3, 0]);
 testStats("stingby", "bee", [1.8, 2.4, 1.6, 2.2, 0]);
-// testStats("dolphin", "dolphin", [2, 2.2, 1.8, 2, 1]);
+testStats("coffer", "frog", [2, 2.2, 1.8, 2, 1]);
+testStats("cow", "radio", [1.6, 2.6, 1.8, 2, 2]);
+testStats("snowman", "starSled", [3, 1, 2.6, 1.4, 2]);
+testStats("penguin", "dolphin", [1.4, 2.8, 1.4, 2.4, 1]);
+testStats("dolphin", "dolphin", [1.2, 3, 1.2, 2.6, 1]);
+testStats("biddy", "biddy", [1, 3.4, .8, 2.8, 0]);
+testStats("chuck", "truck", [3, 1, 2.8, 1.2, 2]);
 
 testRandomDistribution();
 
@@ -78,10 +115,10 @@ testRandomDistribution();
 function testStats(driver, body, expectedStats) {
   Stats.post("getCombo", driver, body)
   .then(combo => {
-    // if (toLvl(combo.lvl[9])  !== expectedStats[0]) throw new Error("spd does not match for combo " + combo.code);
-    if (toLvl(combo.lvl[3])  !== expectedStats[1]) throw new Error("acc does not match for combo " + combo.code);
-    if (toLvl(combo.lvl[4])  !== expectedStats[2]) throw new Error("wgt does not match for combo " + combo.code);
-    // if (toLvl(combo.lvl[10])  !== expectedStats[3]) throw new Error("hnd does not match for combo " + combo.code);
+    // if (toLvl(combo.lvl[statIndex.spd]) !== expectedStats[0]) throw new Error("spd does not match for combo " + combo.code);
+    if (toLvl(combo.lvl[statIndex.acc]) !== expectedStats[1]) throw new Error("acc does not match for combo " + combo.code);
+    if (toLvl(combo.lvl[statIndex.wgt]) !== expectedStats[2]) throw new Error("wgt does not match for combo " + combo.code);
+    // if (toLvl(combo.lvl[statIndex.hnd]) !== expectedStats[3]) throw new Error("hnd does not match for combo " + combo.code);
     if (combo.size !== expectedStats[4]) throw new Error("size does not match for combo " + combo.code);
   });
 }
